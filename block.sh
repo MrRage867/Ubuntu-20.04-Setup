b89e4143a0ea4620a25daa463d8083abdb1e62d3
MrRage <71375812+MrRage867@users.noreply.github.com>
2023-07-20 08:04:30 -0400

    Update block.sh

--- block.sh
+++ block.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-iptables -F
-iptables -A INPUT -s 3.0.0.0/8 -j DROP
-iptables -A OUTPUT -d 3.0.0.0/8 -j DROP
-iptables -N AWS_BLOCK
-for ip_range in $(curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[] | select(.service == "AMAZON") | .ip_prefix'); do
-  iptables -A AWS_BLOCK -s "$ip_range" -j DROP
+# Download the JSON file containing AWS IP ranges
+curl https://ip-ranges.amazonaws.com/ip-ranges.json -o ip-ranges.json
+
+# Use jq to parse the JSON and iterate over the ranges
+cat ip-ranges.json | jq -r '.prefixes[] | .ip_prefix' | while read range; do
+    iptables -A INPUT -s $range -j DROP
 done
 
-iptables -A INPUT -j AWS_BLOCK
+# Remove the temporary JSON file
+rm ip-ranges.json
+
+# Save iptables rules
+iptables-save
